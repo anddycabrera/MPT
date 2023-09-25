7e8d561bd30fd9a58e83a05829e68722f6e70610
anddycabrera <anddycabrera@gmail.com>
2023-09-25 00:11:19 -0400

    update

--- backend/app/api/api_v1/routers/llm.py
+++ backend/app/api/api_v1/routers/llm.py
@@ -31,8 +31,6 @@ async def llmchat(
             "max_new_tokens": "2048",
             "do_sample": "True",
             "num_return_sequences": "1",
-            "eos_token_id": "tokenizer.eos_token_id",
-            "pad_token_id": "tokenizer.eos_token_id",
             "temperature": "0.1",
             "top_p": "0.15",
             "repetition_penalty": "1.2"
