04642b430625b192181a96abc835ee613a506a4e
anddycabrera <anddycabrera@gmail.com>
2023-09-27 19:04:44 -0400

    update

--- backend/app/predictions/client.py
+++ backend/app/predictions/client.py
@@ -73,8 +73,8 @@ async def model_client(FLAGS, prompt_text, model_name = "vllm", sampling_paramet
                 else:
                     output = result.as_numpy("TEXT")
                     for i in output:     
-                        print(type(i))                   
-                        yield i.decode("utf-8")
+                        print(type(i.decode()))                   
+                        yield i.decode()
 
         except InferenceServerException as error:
             print(error)
